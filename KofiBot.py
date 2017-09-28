4a150577f9fca8bf608cdf1c68c464c199bdcafc
Andrés Vieira <anvieiravazquez@gmail.com>
2017-09-28 13:07:43 +0200

    Redone the bot to be a little more sophisticated

--- KofiBot.py
+++ KofiBot.py
@@ -20,27 +20,54 @@ __status__ = "Development"
 
 TOKEN = "360303020:AAGT8eyx7mtda99bL79o02g6G9WcbH4N6eY"
 
+bot = telebot.TeleBot(TOKEN)
+chat_id = -1
+poll_hour = 15
+poll_minute = 50
 
-def main():
-    tb = telebot.TeleBot(TOKEN)
-    chat_id = None
-    while chat_id is None:
-        updates = tb.get_updates()
-        if len(updates) > 0:
-            chat_id = updates[-1].message.chat.id
+
+@bot.message_handler(commands=['changehour'])
+def bot_changehour(message):
+    global chat_id
+
+    if chat_id == -1:
+        bot.reply_to(message, "Please /start the bot first.")
+        return
+
+    new_time = message.text.split("/changehour ")[1]
+    try:
+        hours = int(new_time.split(":")[0])
+        minutes = int(new_time.split(":")[1])
+        if hours in range(0, 24) and minutes in range(0, 60):
+            bot.send_message(chat_id, "Changing hour. New time will be: {}:{}".format(str(hours), str(minutes)))
         else:
-            print "No updates.\n"
+            raise ValueError('Hours or minutes are not correct')
+    except:
+        bot.reply_to(message, "Can't change hour. Please write a correct time. Example: /changehour 18:59")
 
-    print "Achieved a chat id, which is {}".format(chat_id)
-    tb.send_message(chat_id, "Hi there! I'm going to post 'kofipre?' every Monday to Friday at 15:50")
+
+@bot.message_handler(commands=['help'])
+def bot_help(message):
+    bot.reply_to(message, "This are the available commands:\n\n"
+                          "\t/start : Starts the bot. No needed parameters. \n\n"
+                          "\t/help : Shows this message. No needed parameters \n\n"
+                          "\t/changehour XX:XX : Changes the hour. Example: /changehour 15:20")
+
+
+@bot.message_handler(commands=['start'])
+def bot_start(message):
+    """ Starts bot on /start command. Polls time and checks if it has to send a message to the group. """
+    global chat_id
+
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Hi there! I'm KofiBot. My only purpose is to print 'kofipre?' at a defined hour Wednesdays and Thursdays.\n\n"
+                              "I will say 'kofipre?' each day at {}:{}".format(poll_hour, poll_minute))
     while True:
         time.sleep(20)
         d = datetime.today()
-        if d.weekday() in range(0, 5) and d.hour is 15 and d.minute is 50:
-            print "hi"
-            tb.send_message(chat_id, "kofipre?")
+        if d.weekday() in range(2, 4) and d.hour is poll_hour and d.minute is poll_minute:
+            bot.send_message(chat_id, "kofipre?")
             time.sleep(100)
 
-
-if __name__ == '__main__':
-    main()
+print("Started KofiBot")
+bot.polling(none_stop=False, interval=0, timeout=20)
