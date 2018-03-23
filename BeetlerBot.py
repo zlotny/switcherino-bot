0d0d55d8323265cfda82767fcaff4c540b87636b
Andrés Vieira <anvieiravazquez@gmail.com>
2018-03-23 13:36:46 +0100

    Adds README, help handler and gitignore
    The readme file now shows how to install and run.
    
    This commit implements a help/info handler and restructures the
    code a bit to be more readable and PEP8 ready.
    
    Also, added a gitignore to remove IDE/Editor configurations.

--- BeetlerBot.py
+++ BeetlerBot.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" KofiBot
+""" BeetlerBot
 
-The purpose of this bot is just to send 'kofipre?' to a chat at 15:50, Monday to Friday.
+The purpose of this bot is to reply a image of a bee similar to Hitler as a response
+to any message containing a series of keywords.
+
 Yes, seriously.
 """
 
@@ -23,13 +25,29 @@ TOKEN = "527579098:AAGtle1dXS7DBwG3fjnNOZBpS-5YxJn4LpY"
 bot = telebot.TeleBot(TOKEN)
 cid = -1
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+KEYWORDS = ["BEE",
+            "ABEGA",
+            "ABJEA",
+            "ABIEGA",
+            "GABEE",
+            "LAZAJAVIER",
+            "OGABIER"]
+
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
+    ''' Handle /start '''
     bot.reply_to(message, """\
 Hi there, I am BeetlerBot.
 Pay me 25 beetcoins or i will kill you.\
 """)
+
+
+@bot.message_handler(commands=['help', 'info'])
+def send_help(message):
+    ''' Handle /help and /info '''
+    bot.reply_to(
+        message, "Just send a message containing any of this words: {}".format(KEYWORDS))
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
@@ -37,8 +55,9 @@ Pay me 25 beetcoins or i will kill you.\
 def echo_message(message):
 
     cid = message.chat.id
-	
-    if "BEE" in message.text.upper() or "ABEGA" in message.text.upper()	or "ABEJA" in message.text.upper() or "ABIEGA" in message.text.upper() or "GABEE" in message.text.upper() or "LAZAJAVIER" in message.text.upper() or "OGABIER" in message.text.upper(): 
-        bot.send_photo(message.chat.id, open('abega.png', 'rb'),'BZZZZZ')
+
+    if any(x in message.text.upper() for x in KEYWORDS):
+        bot.send_photo(message.chat.id, open('abega.png', 'rb'), 'BZZZZZ')
+
 
 bot.polling()
--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-# KofiBot
-KofiBot is a Telegram bot made in Python2.7 to send a particular message into a conversation. Its not meant to be use otside of my friend group, but in some way its interesting to have it in GitHub for the sake of future reference on how to make a Telegram Bot (?)
+# BeetlerBot
+BeetlerBot is a Telegram bot made in Python2.7 to send a particular message into a conversation. 
 
-KofiBot uses [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI) to work and it's pretty rudimentary.
+Forked from [KofiBot], BeetlerBot send a message with an image and a footnote to as a reply to every message containing a series of keywords.
+
+## Setting up
+```bash
+# Install pyTelegramBotAPI
+pip install pyTelegramBotAPI
+
+# Clone the project
+git clone https://github.com/agnovoa2/BeetlerBot
+
+# Enter the project folder
+cd BeetlerBot
+
+# Ensure your python version is 2.7
+python --version
+```
+
+## Running the software
+```bash
+# If your default python version is 2.7
+python BeetlerBot.py
+
+# If it is not
+python2.7 BeetlerBot.py
+```
+
+BeetlerBot uses [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI) to work.
