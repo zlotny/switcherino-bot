294474c4fb44aa484f834cea82ef8d174ce9eeaf
Andrés Vieira <anvieiravazquez@gmail.com>
2019-09-03 18:17:56 +0200

    Transforms this bot to a golden sun hype one

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# BeetlerBot
-BeetlerBot is a Telegram bot made in Python2.7 to send a particular message into a conversation. 
+# Golden Sun Hype Bot
+Golden Sun Hype Bot is a Telegram bot made in Python 3 to send a particular message into a conversation. 
 
-Forked from [KofiBot], BeetlerBot send a message with an image and a footnote to as a reply to every message containing a series of keywords.
+Based on [BeetlerBot](https://github.com/agnovoa2/BeetlerBot), this bot send a message with an image and a footnote to as a reply to every message containing a series of keywords.
 
 ## Setting up
 ```bash
@@ -9,22 +9,22 @@ Forked from [KofiBot], BeetlerBot send a message with an image and a footnote to
 pip install pyTelegramBotAPI
 
 # Clone the project
-git clone https://github.com/agnovoa2/BeetlerBot
+git clone https://github.com/zlotny/golden-sun-hype-bot
 
 # Enter the project folder
-cd BeetlerBot
+cd golden-sun-hype-bot
 
-# Ensure your python version is 2.7
+# Ensure your python version is 3
 python --version
 ```
 
 ## Running the software
 ```bash
-# If your default python version is 2.7
-python BeetlerBot.py
+# If your default python version is 3
+python main.py
 
 # If it is not
-python2.7 BeetlerBot.py
+python3 main.py
 ```
 
-BeetlerBot uses [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI) to work.
+Golden Sun Hype Bot uses [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI) to work.
--- main.py
+++ main.py
@@ -13,33 +13,26 @@ import telebot
 import time
 from datetime import datetime
 
-__author__ = "Alejandro Gutiérrez"
-__credits__ = ["Andrés Vieira"]
+__author__ = "Andrés Vieira"
+__credits__ = ["Alejandro Gutiérrez"]
 __version__ = "0.1"
-__maintainer__ = "Alejandro Gutiérrez"
-__email__ = "agutierreznovoa90@gmail.com"
+__maintainer__ = "Andrés Vieira"
+__email__ = "anvieiravazquez@gmail.com"
 __status__ = "Development"
 
-TOKEN = "527579098:AAGtle1dXS7DBwG3fjnNOZBpS-5YxJn4LpY"
+TOKEN = "805668801:AAFvl9lmegBE39tM7qjDMN1CCPPnIcepYxo"
 
 bot = telebot.TeleBot(TOKEN)
-cid = -1
 
-KEYWORDS = ["BEE",
-            "ABEGA",
-            "ABEJA",
-            "ABIEGA",
-            "GABEE",
-            "LAZAJAVIER",
-            "OGABIER"]
+KEYWORDS = ["GOLDEN SUN", "LOST AGE", "DARK DOWN"]
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     ''' Handle /start '''
     bot.reply_to(message, """\
-Hi there, I am BeetlerBot.
-Pay me 25 beetcoins or i will kill you.\
+Hi there, I am the Golden Sun Hype bot!.
+Did anyone say Golden Sun?.\
 """)
 
 
@@ -47,17 +40,14 @@ Pay me 25 beetcoins or i will kill you.\
 def send_help(message):
     ''' Handle /help and /info '''
     bot.reply_to(
-        message, "Just send a message containing any of this words: {}".format(KEYWORDS))
+        message, "Just send a message containing any of this words: {}".format(",".join(KEYWORDS)))
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-
-    cid = message.chat.id
-
     if any(x in message.text.upper() for x in KEYWORDS):
-        bot.send_photo(message.chat.id, open('abega.png', 'rb'), 'BZZZZZ')
+        bot.send_photo(message.chat.id, open('hype.png', 'rb'), 'ALGUIEN HA DICHO... G O L D E N S U N ?')
 
 while True:
     try:
